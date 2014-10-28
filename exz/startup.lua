29f250104ba44ec309d92d5b8f9943b86c0574c5
Jaypei <jaypei97159@gmail.com>
2014-10-28 12:08:50 +0800

    fix main screen

--- exz/startup.lua
+++ exz/startup.lua
@@ -1,3 +1,5 @@
+local awful = require("awful")
+awful.util = require("awful.util")
 local utils = require("exz.utils")
 local os = os
 
@@ -8,8 +10,12 @@ os.execute("pkill compton")
 
 -- should disable the fcitx option OverrideSystemXKBSettings
 utils.sexec("/usr/bin/xmodmap ~/.xmodmaprc")
-utils.sexec_once("parcellite")
+-- utils.sexec_once("parcellite")
 utils.sexec_once("kbdd")
--- utils.sexec_once("compton")
+utils.sexec_once("compton")
 utils.sexec_once("gnome-sound-applet")
 utils.sexec_once("shutter --min_at_startup &")
+
+-- desktop_start_id = os.getenv("DESKTOP_AUTOSTART_ID")
+-- register_command = "dbus-send --session --print-reply=literal --dest=org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.RegisterClient string:awesome string:" .. desktop_start_id
+-- awful.util.spawn_with_shell(register_command)
