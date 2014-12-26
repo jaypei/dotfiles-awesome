b2c525ab415819eed7baaee2c328eb8feead8aea
jaypei <jaypei97159@gmail.om>
2014-12-26 23:15:36 +0800

    remove vain

--- exz/tag.lua
+++ exz/tag.lua
@@ -4,29 +4,29 @@ require("awful.tag")
 local awful = require("awful")
 local screen = screen
 local ipairs = ipairs
-local vain = require("vain")
+local lain = require("lain")
 local config = require("exz.config")
 
 module("exz.tag")
 
 -- Window management layouts
 layouts = {
-   vain.layout.termfair,       -- 1
-   vain.layout.browse,         -- 2
-   vain.layout.cascadebrowse,  -- 3
-   vain.layout.uselessfair,    -- 4
-   vain.layout.uselesstile,    -- 5
-   vain.layout.cascade,        -- 6
-   vain.layout.centerwork      -- 7
+   lain.layout.termfair,       -- 1
+   lain.layout.uselessfair,         -- 2
+   lain.layout.uselessfair,  -- 3
+   lain.layout.uselessfair,    -- 4
+   lain.layout.uselesstile,    -- 5
+   lain.layout.cascade,        -- 6
+   lain.layout.centerwork      -- 7
 }
 
 tags = {
   names  = { "term", "emacs", "web",
              "mail", "fs", "doc",
              7, "gimp", "vm" },
-  layout = { vain.layout.termfair, vain.layout.centerwork, layouts[2],
+  layout = { lain.layout.termfair, lain.layout.centerwork, layouts[2],
              layouts[4], layouts[2], layouts[2],
-             layouts[2], vain.layout.gimp, layouts[4]
+             layouts[2], lain.layout.uselessfair, layouts[4]
 }}
 
 for s = 1, screen.count() do
@@ -35,7 +35,7 @@ for s = 1, screen.count() do
       awful.tag.setproperty(t, "mwfact", i==8 and 0.13  or  0.5)
       awful.tag.setproperty(t, "hide",  i==7 and true)
   end
-  awful.layout.set(vain.layout.termfair, tags[s][1])
+  awful.layout.set(lain.layout.termfair, tags[s][1])
   awful.tag.setnmaster(2, tags[s][1])
   awful.tag.setncol(1, tags[s][1])
   awful.screen.padding(screen[s], config.screen_padding)
