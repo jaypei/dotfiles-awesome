14d53887fba927b5c99f0422b66e4e7df999ccbb
jaypei <jaypei97159@gmail.om>
2015-01-24 00:53:53 +0800

    Notification style

--- exz/notify.lua
+++ exz/notify.lua
@@ -5,6 +5,11 @@ local naughty = require("naughty")
 module("exz.notify")
 
 naughty.config.defaults.screen = config.main_screen
+naughty.config.defaults.timeout = 0
+naughty.config.defaults.font = "Fantasque Sans Mono 20px"
+naughty.config.defaults.bg = "#551111"
+naughty.config.defaults.fg = "#FFFFFF"
+naughty.config.defaults.border_color = "#FF0000"
 
 function info(title, text)
    naughty.notify({ preset = naughty.config.presets.info,
